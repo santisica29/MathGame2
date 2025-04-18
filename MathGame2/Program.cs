c9421775c922ee85226ac55a512caf8d471afaf5
Santiago Sica <santisica29@gmail.com>
2025-04-18 17:59:21 -0300

    initial math game structure

--- MathGame2/Program.cs
+++ MathGame2/Program.cs
@@ -1,5 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("I understand it now...");
-Console.WriteLine("Let's do it");
+﻿Console.WriteLine("Please type your name");
 
-Console.WriteLine("doggies");
+var name = Console.ReadLine();
+var date = DateTime.Now;
+
+Menu(name, date);
+
+void QuitGame()
+{
+    Console.WriteLine("Goodbye");
+    Environment.Exit(1);
+}
+void AdditionGame(string message)
+{
+    Console.WriteLine(message);
+}
+void SubstractionGame(string message)
+{
+    Console.WriteLine(message);
+}
+void MultiplicationGame(string message)
+{
+    Console.WriteLine(message);
+}
+void DivisionGame(string message)
+{
+    Console.WriteLine(message);
+}
+
+void Menu(string? name, DateTime date)
+{
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine($"Hello {name.ToUpper()}. It's {date}. This is your math's game. Let's get started.\n");
+    Console.WriteLine($@"What game would you like to play today?:
+A - Addition
+S - Substraction
+M - Multiplication
+D - Division
+Q - Quit
+");
+    Console.WriteLine("---------------------------");
+
+    var userGameSelected = Console.ReadLine().Trim().ToLower();
+
+    switch (userGameSelected)
+    {
+        case "a":
+            AdditionGame("Addition selected");
+            break;
+        case "s":
+            SubstractionGame("Substraction game selected");
+            break;
+        case "m":
+            MultiplicationGame("Multiplication game selected");
+            break;
+        case "d":
+            DivisionGame("Division game selected");
+            break;
+        case "q":
+            QuitGame();
+            break;
+        default:
+            Console.WriteLine($"Incorrect input: {userGameSelected}");
+            break;
+    }
+}
